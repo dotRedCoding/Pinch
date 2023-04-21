b355b845c8c95b51c889ac7a5e7a3537efa4f1cd
dotRedCode <dotredcoding@gmail.com>
2023-04-21 10:44:22 -0400

    - Drag Gesture created - function created to reset all values back to their default implementation when animation has ended

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -11,14 +11,23 @@ struct ContentView: View {
     // MARK: - PROPERTY
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
     
     // MARK: - FUNCTION
+    
+    func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+        
+    }
     
     // MARK: - CONTENT
     var body: some View {
         NavigationView {
             ZStack {
-                // MAK: - Page Image
+                // MARK: - Page Image
                 Image("magazine-front-cover")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
@@ -26,6 +35,7 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height) // order matters - drag must come first
                     .scaleEffect(imageScale)
                 // MARK: - 1. TAP GESTURE
                 // count 2 refers to the amount of taps
@@ -35,11 +45,23 @@ struct ContentView: View {
                                 imageScale = 5 // scale up if default value is 1
                             }
                         } else { // if image is already scaled up go back to default state
-                            withAnimation(.spring()) {
-                                imageScale = 1
-                            }
+                            resetImageState()
                         }
                     })
+                // MARK: - 2. DRAG GESTURE
+                    .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                            withAnimation(.linear(duration: 1)) {
+                                imageOffset = value.translation
+                            }
+                        }
+                        .onEnded { _ in // we do not care about the transtion value we want it to reset
+                            if imageScale <= 1 {
+                                resetImageState()
+                            }
+                        }
+                    )
                 
             } // End of Zstack
             .navigationTitle("Pinch & Zoom")
